ad712e49acc5a162147d85d3ad614134a467868e
Goce Georgievski <mr.goce@gmail.com>
2024-05-19 10:41:32 +0200

    test

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo_rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "devstaccount0101984"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "dev-terraform"                          # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "demo_rg"
+    storage_account_name = "devstaccount0101984"
+    container_name       = "dev-terraform"
+    key                  = "prod.terraform.tfstate"
   }
 }
