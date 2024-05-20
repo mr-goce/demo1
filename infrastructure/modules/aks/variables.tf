1c375a14b16536ae39d8048474981a6c5d9d9c01
Goce Georgievski <mr.goce@gmail.com>
2024-05-20 10:29:49 +0200

    test

---
+++ infrastructure/modules/aks/variables.tf
@@ -0,0 +1,20 @@
+# variable "location" {
+#   type = string
+# }
+# # variable "resource_group_name" {}
+
+# variable "service_principal_name" {
+#   type = string
+# }
+
+# # variable "ssh_public_key" {
+# #   default = "~/.ssh/id_rsa.pub"
+# # }
+# variable "aks_resource_group_name" {
+#     type =  string
+# }
+# variable "client_id" {}
+# variable "client_secret" {
+#   type = string
+#   sensitive = true
+# }
